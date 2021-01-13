ff2a5140147a05f904500f3fe5367420ff0b3dbb
lucio rendine <luciorendine69@gmail.com>
2021-01-13 23:39:59 +0100

    last  stand

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
 $(document).ready(function(){
-
+   // freccia destra
    $('.next').click(function() {
      var activeImg = $('.images img.active');
+         activeImg.removeClass('active');
+
      if (activeImg.hasClass('last')) {
         var firstImage = $('.images img.first');
         firstImage.addClass('active');
-        activeImg.removeClass('active');
      } else {
           activeImg.removeClass('active').next().addClass('active');
      }
    })
+    // freccia sinistra
+   $('.prev').click(function() {
+     var activeImgTwo = $('.images img.active');
+     activeImgTwo.removeClass('active');
 
-   $('.fa-angle-left').click(function() {
-     $('img.active').removeClass('active').next().addClass('active');
+     if (activeImgTwo.hasClass('first')) {
+        var lastImage = $('.images img.last');
+        lastImage.addClass('active');
+     } else {
+       activeImgTwo.removeClass('active').prev().addClass('active');
+     }
    })
+
+   $('.next').click(function() {
+     var activeDot = $('.nav .fa-circle.active');
+         activeDot.removeClass('active');
+
+     if (activeDot.hasClass('last')) {
+        var firstDot = $('.nav .fa-circle.first');
+        firstDot.addClass('active');
+     } else {
+          activeDot.removeClass('active').next().addClass('active');
+     }
+   })
+
+   $('.prev').click(function() {
+     var activeDotTwo = $('.nav .fa-circle.active');
+     activeDotTwo.removeClass('active');
+
+     if (activeDotTwo.hasClass('first')) {
+        var lastDot = $('.nav .fa-circle.last');
+        lastDot.addClass('active');
+     } else {
+       activeDotTwo.removeClass('active').prev().addClass('active');
+     }
+   })
+
+
 
 })
