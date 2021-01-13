d7e5f311fceb501cf7da0c3a0a19398d58b3f4c3
lucio rendine <luciorendine69@gmail.com>
2021-01-13 18:04:04 +0100

    push

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
 
-   $('.fa-angle-right').click(function() {
-     $('img.active').removeClass('active').next().addClass('active');
-     if ('active' === 'first') {
-       $('first').show()
+   $('.next').click(function() {
+     var activeImg = $('.images img.active');
+     if (activeImg.hasClass('last')) {
+        var firstImage = $('.images img.first');
+        firstImage.addClass('active');
+        activeImg.removeClass('active');
+     } else {
+          activeImg.removeClass('active').next().addClass('active');
      }
    })
 
    $('.fa-angle-left').click(function() {
      $('img.active').removeClass('active').next().addClass('active');
-     if ('active' === 'last') {
-       $('last').show()
-     }
    })
 
 })
